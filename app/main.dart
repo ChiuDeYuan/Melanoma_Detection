2dc6f92cde4f6354cc2b8862d8c41eb137ed37c4
ChiuChiuCircle <118914606+ChiuDeYuan@users.noreply.github.com>
2024-10-20 16:56:25 +0800

    Add files via upload

---
+++ app/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import './screens/melanoma_app.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          colorSchemeSeed: const Color(0xff6750a4), useMaterial3: true),
+      home: const MelanomaApp(),
+    );
+  }
+}
